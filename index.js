fa6517c9ca812d578646d1940e7d5d168df97693
Christopher Higgins <code@chiggins.me>
2024-02-18 14:55:46 -0700

    Add note format

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 "use strict";
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
-const fields = ["word", "pronunciation", "definition"];
+const fields = [
+    "word",
+    "partOfSpeech",
+    "pronunciation1",
+    "audio1",
+    "pronunciation2",
+    "audio2",
+    "pronunciation3",
+    "audio3",
+    "definition1",
+    "example1",
+    "definition2",
+    "example2",
+    "definition3",
+    "example3",
+    "definition4",
+    "example4",
+    "definition5",
+    "example5",
+    "definition6",
+    "example6",
+    "definition7",
+    "example7",
+    "definition8",
+    "example8",
+    "definition9",
+    "example9",
+    "definition10",
+    "example10",
+    "synonyms",
+    "antonyms",
+];
 const headers = {
     separator: ";",
     html: true,
     columns: fields.join(";"),
 };
 Promise.allSettled(process.argv.splice(2).map((arg) => {
-    return fetch(`${url}/${arg}`)
+    return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${arg}`)
         .then((res) => {
-        if (res.ok) {
-            return res.json().then(makeCard);
-        }
-        else {
+        if (!res.ok)
             throw arg;
-        }
+        return res.json().then(makeNotes);
     })
         .catch(() => {
         throw arg;
@@ -25,7 +52,7 @@ Promise.allSettled(process.argv.splice(2).map((arg) => {
             fail.push(promise.reason);
         }
         else {
-            success.push(promise.value);
+            success.push(...promise.value);
         }
         return [success, fail];
     }, [[], []]);
@@ -42,16 +69,49 @@ Promise.allSettled(process.argv.splice(2).map((arg) => {
         console.error(`Failed to fetch: ${failedWords.join(" ")}`);
     }
 });
-function makeCard([dict]) {
-    // this is to make it typesafe with the headers
-    const thisFields = {
-        word: dict.word,
-        pronunciation: dict.phonetic,
-        definition: 'something "quoted" yea',
-    };
-    return Object.values(thisFields).map(escapeField).join(";");
+function makeNotes(entries) {
+    return entries.flatMap((dict) => {
+        // remove non-us audio and phonetics with no audio (they're usually non-us)
+        dict.phonetics = dict.phonetics?.filter((p) => p.audio?.includes("-us."));
+        return dict.meanings.map((meaning) => {
+            // this is to make it typesafe with the headers.
+            const thisFields = {
+                [fields[0]]: dict.word,
+                [fields[1]]: meaning.partOfSpeech,
+                [fields[2]]: dict.phonetics?.[0]?.text || dict.phonetic,
+                [fields[3]]: dict.phonetics?.[0]?.audio,
+                [fields[4]]: dict.phonetics?.[1]?.text,
+                [fields[5]]: dict.phonetics?.[1]?.audio,
+                [fields[6]]: dict.phonetics?.[2]?.text,
+                [fields[7]]: dict.phonetics?.[2]?.audio,
+                [fields[8]]: meaning.definitions[0]?.definition,
+                [fields[9]]: meaning.definitions[0]?.example,
+                [fields[10]]: meaning.definitions[1]?.definition,
+                [fields[11]]: meaning.definitions[1]?.example,
+                [fields[12]]: meaning.definitions[2]?.definition,
+                [fields[13]]: meaning.definitions[2]?.example,
+                [fields[14]]: meaning.definitions[3]?.definition,
+                [fields[15]]: meaning.definitions[3]?.example,
+                [fields[16]]: meaning.definitions[4]?.definition,
+                [fields[17]]: meaning.definitions[4]?.example,
+                [fields[18]]: meaning.definitions[5]?.definition,
+                [fields[19]]: meaning.definitions[5]?.example,
+                [fields[20]]: meaning.definitions[6]?.definition,
+                [fields[21]]: meaning.definitions[6]?.example,
+                [fields[22]]: meaning.definitions[7]?.definition,
+                [fields[23]]: meaning.definitions[7]?.example,
+                [fields[24]]: meaning.definitions[8]?.definition,
+                [fields[25]]: meaning.definitions[8]?.example,
+                [fields[26]]: meaning.definitions[9]?.definition,
+                [fields[27]]: meaning.definitions[9]?.example,
+                [fields[28]]: meaning.synonyms.join(", "),
+                [fields[29]]: meaning.antonyms.join(", "),
+            };
+            return Object.values(thisFields).map(escapeField).join(headers.separator);
+        });
+    });
 }
 function escapeField(field) {
     // replace " with "" to escape the double quotes. Then wrap whole thing in double quotes
-    return `"${field.replaceAll('"', '""')}"`;
+    return `"${(field || "").replaceAll('"', '""')}"`;
 }
